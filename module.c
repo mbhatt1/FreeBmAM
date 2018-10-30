7ef0c27538b78b5a3ec3909c3cfb9006b4b453f7
Alex "The Sage <mbhatt@uno.edu>
2018-10-30 15:39:04 -0500

    Update module.c

--- module.c
+++ module.c
@@ -58,7 +58,7 @@ int dump_memory(){
 		vm_page_t virtual_address;
 		virtual_address = pmap_kenter_temporary(dst, 0);
 		if (virtual_address != 0){
-		memcpy( page, (void *) virtual_address, PAGE_SIZE);
+		//memcpy( page, (void *) virtual_address, PAGE_SIZE);
 		uprintf("Copied Page 0x%x\t", dst);
 		error = kio_write(vp, virtual_address, PAGE_SIZE,offset);
                 offset = dst;
