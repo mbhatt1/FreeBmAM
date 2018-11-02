9b4304168abd71aa48429caf33cc2534e1227edb
Alex "The Sage <mbhatt@uno.edu>
2018-11-02 01:40:22 -0500

    Update module.c

--- module.c
+++ module.c
@@ -53,14 +53,14 @@ int dump_memory(){
 	unsigned long dst = 0;
 	int error = 0;
         unsigned long offset = 0;
-
+	void * page = malloc (PAGE_SIZE, M_PAGE, M_NOWAIT | M_USE_RESERVE);
 	for (int i = 0; i < page_count; i++){
 		vm_page_t virtual_address;
 		virtual_address = pmap_kenter_temporary(trunc_page(dst), 0);
 		if (virtual_address != 0){
-		//memcpy( page, (void *) virtual_address, PAGE_SIZE);
-		//uprintf("Copied Page 0x%x\t", dst);
-		error = kio_write(vp, virtual_address, PAGE_SIZE, offset);
+		memcpy( page, (void *) virtual_address, PAGE_SIZE);
+		uprintf("Copied Page 0x%x\t", dst);
+		error = kio_write(vp, page, PAGE_SIZE, offset);
 		if (error != 0){
 			uprintf("Your file path may not be correct. This module does not create directories that do not exist. Please FIX PATH\n");
 			return -1;
